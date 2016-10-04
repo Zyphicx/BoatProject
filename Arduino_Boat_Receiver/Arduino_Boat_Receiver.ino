8fb8da7f02a75aa22afbe716e3ceca9dce0162ca
Elever i 8D <Elever i 8D>
2016-10-04 10:53:04 +0300

    Initial version

---
+++ Arduino_Boat_Receiver/Arduino_Boat_Receiver.ino
@@ -0,0 +1,42 @@
+const int TRANS = 4;
+const int MOTOR = 6;
+
+const int powerTwo = 6;
+
+void setup() {
+  pinMode(TRANS, INPUT);
+  pinMode(MOTOR, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+    if(digitalRead(TRANS) == HIGH){
+      delayMicroseconds(1500);
+      int signalValue = getSignal(TRANS);
+      Serial.print("Value: ");
+      Serial.println(signalValue);
+      Serial.println("-----------");
+      if(signalValue == 2){
+        analogWrite(MOTOR, 255);
+      }else{
+        analogWrite(MOTOR, 150);
+      }
+    }else{
+      analogWrite(MOTOR, 150);  
+    }
+}
+
+int getSignal(int pin){
+  int value = 0;
+  int i;
+  int power = 1;
+  
+  for(i = 0; i < powerTwo; i++){
+    int pulseState = digitalRead(pin);
+    //Serial.println(pulseState);
+    value += power * pulseState;
+    power *= 2;
+    delay(1);
+  }
+  return value;
+}
---
+++ Arduino_Boat_Sender/Arduino_Boat_Sender.ino
@@ -0,0 +1,38 @@
+const int LED = 4;
+const int powerTwo = 6;
+const int maxPower = 32;
+
+int randomValue;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  int sig[powerTwo];
+  int valueToSend = 2;
+  Serial.println(valueToSend);
+  valueToSignal(valueToSend, sig);
+  sendSignal(LED, sig);
+  delay(100);
+}
+
+void sendSignal(int pin, int *sig){
+  digitalWrite(pin, HIGH);
+  delay(1);
+  for(int i = 0; i < powerTwo; i++){
+      digitalWrite(pin, sig[i]);
+      delay(1);
+  }
+  digitalWrite(pin, LOW);
+}
+
+void valueToSignal(int value, int *sig){
+  int power;
+  int i = powerTwo - 1;
+  
+  for(power = maxPower; power >= 1; power /= 2){
+    value -= ((sig[i--] = ((int) value / power)) ? power : 0);
+  }
+}
