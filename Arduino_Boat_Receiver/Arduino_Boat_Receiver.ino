a2b2575f85bae9b6dd445a1b59da91c31ee1dd13
Zyphicx <zyphicxs@gmail.com>
2017-01-31 22:14:58 +0200

    Added button support

--- Arduino_Boat_Receiver/Arduino_Boat_Receiver.ino
+++ Arduino_Boat_Receiver/Arduino_Boat_Receiver.ino
@@ -3,10 +3,13 @@ struct{ //A structure for each side of the boat
   const int MOTORPIN;
   int SIGNALS[10];
   int curSig;
-} RCVS[] = {4, 6, {6}, 2,
-            5, 7, {5}, 3};
+} RCVS[] = {4, 6, {2, 3, 4}, 2,
+            5, 7, {5, 6, 7}, 5};
 
 const int powerTwo = 6;
+
+const int buttonPin = 11;
+int route = 1;
 
 void setup() {
   int i;
@@ -14,30 +17,36 @@ void setup() {
     pinMode(RCVS[i].TRANSPIN, INPUT);
     pinMode(RCVS[i].MOTORPIN, OUTPUT);
   }
-  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600); //YOU SHOULD PROBABLY REMOVE THIS PIECE OF CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 
 void loop(){
-  int i;
-  for(i = 0; i < sizeof(RCVS) / sizeof(RCVS[0]); i++){
+    if(digitalRead(buttonPin) == HIGH)
+        route = (route ? 0 : 1);
+    digitalWrite(); //MODIFY THIS TO CHANGE THE DIRECTION THAT THE MOTOR IS GOING IN (YOU COULD ALSO JUST CHANGE IT IN THE IF-STATEMENT)
+    digitalWrite();
+    
+    int i;
+    for(i = 0; i < sizeof(RCVS) / sizeof(RCVS[0]); i++){
       if(digitalRead(RCVS[i].TRANSPIN) == HIGH){
         delayMicroseconds(1500);
-        int signalValue = getSignal(RCVS[i].TRANSPIN);
-        Serial.print("Value: ");
-        Serial.println(signalValue);
-        Serial.println("-----------");
-        if(contains(RCVS[i].SIGNALS, signalValue, sizeof(RCVS[i].SIGNALS) / sizeof(RCVS[0].SIGNALS[0])) && signalValue >= RCVS[i].curSig){
-          Serial.println("Running");
-          if(signalValue > RCVS[i].curSig)
-             RCVS[i].curSig = signalValue;
-          analogWrite(RCVS[i].MOTORPIN, 255);
-          delay(25);
-        }else{
-          analogWrite(RCVS[i].MOTORPIN, 127); //CHANGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        }
-      }else{
-        analogWrite(RCVS[i].MOTORPIN, 127); //CHANGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      }
+          int signalValue = getSignal(RCVS[i].TRANSPIN);
+          Serial.print("Value: ");         //AND ALL OF THESE LINES OF CODE!!!!!!!!!!!!!!!!
+          Serial.println(signalValue);
+          Serial.println("-----------");
+          if(contains(RCVS[i].SIGNALS, signalValue, sizeof(RCVS[i].SIGNALS) / sizeof(RCVS[0].SIGNALS[0])) && signalValue >= RCVS[i].curSig){
+            Serial.println("Running"); // AND THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            if(signalValue > RCVS[i].curSig)
+              RCVS[i].curSig = signalValue;
+            analogWrite(RCVS[i].MOTORPIN, 255);
+            delay(50);
+          }else{
+            analogWrite(RCVS[i].MOTORPIN, 150); //CHANGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          }
+       }else{
+        analogWrite(RCVS[i].MOTORPIN, 150); //CHANGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
   }
 }
 
@@ -45,7 +54,7 @@ int getSignal(int pin){
   int value = 0;
   int i;
   int power = 1;
-  
+
   for(i = 0; i < powerTwo; i++){
     int pulseState = digitalRead(pin);
     //Serial.println(pulseState);
